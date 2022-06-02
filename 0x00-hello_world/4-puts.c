1862284808794763304780adc614d88becaed0d8
edward <letebeleM98@gmail.com>
2022-06-02 14:54:03 -0700

    My first C programme

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,14 @@
 #include <stdio.h>
 
-
-
-int main(void)
-
 /**
- * main - Entry point
+ * main-Entry point
+ * 
+ * Description:prints "Programming is like building a multilingual puzzle"
  *
  * Return: Always 0 (Success)
  */
-
-
-  { 
-
-
-	  puts("\"Programming is like building a multilingual puzzle");
-		  return (0);
-
-
-
-  }
-
+int main(void
+		{
+		puts("\"Programming is like building a multilingualpuzzle");
+		return (0);
+		}
