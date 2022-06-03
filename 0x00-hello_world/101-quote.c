3c5b91de7dac9252a8ded2236db32535bc78e8ba
edward <letebeleM98@gmail.com>
2022-06-02 18:56:41 -0700

    Commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 
 /**
  * main - Entry point
  *
- * Return: Always 1 (Success)
+ * Return: Always 1 (failure)
  *
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (1);
 }
