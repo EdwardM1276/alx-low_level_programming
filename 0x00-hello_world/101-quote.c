829a02fbb73924c903bdac278037dc68ef18a6f7
edward <letebeleM98@gmail.com>
2022-06-02 20:14:37 -0700

    My C Programme

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,4 @@
-#include <stdio.h>
 #include <unistd.h>
-#include <stdlib.h>
 
 /**
  * main - Entry point
@@ -10,6 +8,7 @@
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+char str1[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2,str1,59);
+return (1);
 }
